e182df9ffe8f462220ff7899cd946798348d0024
AndreuMiroSabate <andreu.miro@estudiantat.upc.edu>
2022-10-26 13:32:10 +0200

    Map_Actualisation

---
+++ Output/Assets/Maps/landscape_destroy_pixel_art_by_yoshift_db7csz7-fullview.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="landscape_destroy_pixel_art_by_yoshift_db7csz7-fullview" tilewidth="32" tileheight="32" tilecount="200" columns="20">
+ <image source="../../../../../../Downloads/landscape_destroy_pixel_art_by_yoshift_db7csz7-fullview.jpg" width="640" height="350"/>
+</tileset>
---
+++ Output/Assets/Maps/metal panels.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="metal panels" tilewidth="32" tileheight="32" tilecount="20" columns="10">
+ <image source="metal panels.png" width="320" height="64"/>
+</tileset>
