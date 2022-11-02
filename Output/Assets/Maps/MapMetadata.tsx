da553f414df09c39f35501888183757abe2a27c7
AndreuMiroSabate <andreu.miro@estudiantat.upc.edu>
2022-11-02 12:51:42 +0100

    Actu Map

---
+++ Output/Assets/Maps/MapMetadata.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="MapMetadata" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="2" columns="2">
+ <image source="MapMetadata.png" width="67" height="34"/>
+</tileset>
