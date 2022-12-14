481bebb10f0f4fc177bb125b7a204536c9dff2ab
flishflash <jose.antonio.tur@estudiantat.upc.edu>
2022-12-14 17:45:52 +0100

    fuera!!!

--- Output/Assets/Maps/Ground.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="Ground" tilewidth="32" tileheight="32" tilecount="180" columns="18">
- <image source="Tileset.png" width="576" height="320"/>
-</tileset>
